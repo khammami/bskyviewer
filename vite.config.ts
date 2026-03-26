50e9e28bca4a60f80669a49fabb287bbd2580bcb
Khalil Hammami <khammami@gmail.com>
2026-03-26 11:52:55 +0100

    feat: enhance Vite configuration with build options and manual chunking

--- vite.config.ts
+++ vite.config.ts
@@ -2,4 +2,22 @@ import react from '@vitejs/plugin-react-swc'
 import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
-export default defineConfig({ plugins: [react()] })
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    chunkSizeWarningLimit: 1000,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          'vendor-react': [
+            'react',
+            'react-dom',
+            'react-router',
+            'react-tooltip',
+          ],
+          'vendor-atproto': ['@atproto/api'],
+        },
+      },
+    },
+  },
+})
